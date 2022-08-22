caee29301d063c049ab3b5826a391ea1a6b3336c
Romain Vacheret <romain.vacheret@gmail.com>
2022-08-22 10:52:30 +0200

    refactor: use bools instead of chars to represent the qrcode matrix

--- main.c
+++ main.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <strings.h>
 
 #define VERSION 1
 #define WIDTH_SIZE 21
 
 typedef struct {
-    char values [WIDTH_SIZE * WIDTH_SIZE];
-    char are_empty[WIDTH_SIZE * WIDTH_SIZE];
+    bool values [WIDTH_SIZE * WIDTH_SIZE];
+    bool are_empty[WIDTH_SIZE * WIDTH_SIZE];
 } QRCode;
 
 QRCode qrcode;
 
 void qrcode_init() {
-    memset(qrcode.values, ' ', WIDTH_SIZE * WIDTH_SIZE);
-    memset(qrcode.are_empty, ' ', WIDTH_SIZE * WIDTH_SIZE);
+    memset(qrcode.values, false, WIDTH_SIZE * WIDTH_SIZE);
+    memset(qrcode.are_empty, false, WIDTH_SIZE * WIDTH_SIZE);
 }
 
-void utils_display_matrix(FILE* stream, char string[], int is_qrcode) {
+void utils_display_matrix(FILE* stream, bool matrix[], bool is_qrcode) {
     char info_message[64];
 
     sprintf(
@@ -29,7 +30,7 @@ void utils_display_matrix(FILE* stream, char string[], int is_qrcode) {
     fputs(info_message, stream);
 
     for(size_t i = 0; i < WIDTH_SIZE * WIDTH_SIZE; i++) {
-        fputc(string[i], stream);
+        fputc(" #"[matrix[i]], stream);
         fputc(' ', stream);
 
         if(i && (i + 1) % WIDTH_SIZE == 0) {
@@ -39,47 +40,47 @@ void utils_display_matrix(FILE* stream, char string[], int is_qrcode) {
 }
 
 void utils_display_availability_mask(FILE* stream) {
-    utils_display_matrix(stream, qrcode.are_empty, 0);
+    utils_display_matrix(stream, qrcode.are_empty, false);
 }
 
 void qrcode_display(FILE* stream) {
-    utils_display_matrix(stream, qrcode.values, 1);
+    utils_display_matrix(stream, qrcode.values, true);
 }
 
 // TODO: Check bounds beforehand
-void utils_set_square_ring(size_t top_left_index, size_t width, char string[], char value) {
+void utils_set_square_ring(size_t top_left_index, size_t width, bool matrix[], bool value) {
     for(size_t i = 0; i < width - 1; i++) {
         // top left  -> top right - 1
-        string[top_left_index + i] = value; 
+        matrix[top_left_index + i] = value; 
         // top right -> bottom right - 1
-        string[top_left_index + (width - 1) + (WIDTH_SIZE * i)] = value;
+        matrix[top_left_index + (width - 1) + (WIDTH_SIZE * i)] = value;
         // top left + 1 -> bottom left
-        string[top_left_index + (WIDTH_SIZE * (i + 1))] = value;
+        matrix[top_left_index + (WIDTH_SIZE * (i + 1))] = value;
         // bottom right -> bottom left - 1
-        string[top_left_index + (width - 1) + ((width - 1) * WIDTH_SIZE) - i] = value; 
+        matrix[top_left_index + (width - 1) + ((width - 1) * WIDTH_SIZE) - i] = value; 
     }
 }
 
-void utils_set_vertical_line(size_t column, size_t start_row, size_t end_row, char string[], char value) {
+void utils_set_vertical_line(size_t column, size_t start_row, size_t end_row, bool matrix[], bool value) {
     for(size_t i = 0; i < end_row - start_row + 1; i++) {
-        string[column + (start_row + i) * WIDTH_SIZE] = value;
+        matrix[column + (start_row + i) * WIDTH_SIZE] = value;
     }
 }
 
-void utils_set_horizontal_line(size_t row, size_t start_column, size_t end_column, char string[], char value) {
+void utils_set_horizontal_line(size_t row, size_t start_column, size_t end_column, bool matrix[], bool value) {
     for(size_t i = 0; i < end_column - start_column + 1; i++) {
-        string[row * WIDTH_SIZE + start_column + i] = value;
+        matrix[row * WIDTH_SIZE + start_column + i] = value;
     }
 }
 
-void pattern_set_finder_pattern(size_t initial_idx, char string[], char value) {
-    utils_set_square_ring(initial_idx, 7, string, value);
-    utils_set_square_ring(initial_idx + 2 + 2 * WIDTH_SIZE, 3, string, value);
-    string[initial_idx + 3 + 3 * WIDTH_SIZE] = value;
+void pattern_set_finder_pattern(size_t initial_idx, bool matrix[], bool value) {
+    utils_set_square_ring(initial_idx, 7, matrix, value);
+    utils_set_square_ring(initial_idx + 2 + 2 * WIDTH_SIZE, 3, matrix, value);
+    matrix[initial_idx + 3 + 3 * WIDTH_SIZE] = value;
 }
 
 void pattern_set_finder(size_t initial_idx) {
-    pattern_set_finder_pattern(initial_idx, qrcode.values, '#');
+    pattern_set_finder_pattern(initial_idx, qrcode.values, true);
 }
 
 void pattern_set_all_finders() {
@@ -89,8 +90,8 @@ void pattern_set_all_finders() {
 }
 
 void pattern_reserve_finder(size_t initial_idx) {
-    pattern_set_finder_pattern(initial_idx, qrcode.are_empty, '#');
-    utils_set_square_ring(initial_idx + 1 + WIDTH_SIZE, 5, qrcode.are_empty, '#');
+    pattern_set_finder_pattern(initial_idx, qrcode.are_empty, true);
+    utils_set_square_ring(initial_idx + 1 + WIDTH_SIZE, 5, qrcode.are_empty, true);
 }
 
 void pattern_reserve_all_finders() {
@@ -103,12 +104,12 @@ void pattern_reserve_all_patterns() {
     pattern_reserve_all_finders();
     
     // separators for finder patterns
-    utils_set_horizontal_line(7, 0, 7, qrcode.are_empty, '#'); 
-    utils_set_horizontal_line(7, 13, 20, qrcode.are_empty, '#'); 
-    utils_set_horizontal_line(13, 0, 7, qrcode.are_empty, '#'); 
-    utils_set_vertical_line(7, 0, 6, qrcode.are_empty, '#'); 
-    utils_set_vertical_line(7, 13, 20, qrcode.are_empty, '#'); 
-    utils_set_vertical_line(13, 0, 6, qrcode.are_empty, '#'); 
+    utils_set_horizontal_line(7, 0, 7, qrcode.are_empty, true); 
+    utils_set_horizontal_line(7, 13, 20, qrcode.are_empty, true); 
+    utils_set_horizontal_line(13, 0, 7, qrcode.are_empty, true); 
+    utils_set_vertical_line(7, 0, 6, qrcode.are_empty, true); 
+    utils_set_vertical_line(7, 13, 20, qrcode.are_empty, true); 
+    utils_set_vertical_line(13, 0, 6, qrcode.are_empty, true); 
 }
 
 size_t utils_get_next_idx(size_t current_idx) {
@@ -130,7 +131,7 @@ size_t utils_get_next_available_idx(size_t current_idx) {
 
     do {
         next_idx = utils_get_next_idx(current_idx);
-        is_available = qrcode.are_empty[next_idx] == ' ';
+        is_available = qrcode.are_empty[next_idx] == false;
     } while(!is_available);
 
     return next_idx;
